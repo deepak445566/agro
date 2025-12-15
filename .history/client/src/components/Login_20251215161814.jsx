a994c3fe23cf2dca56ce48ce79e897e48b5efcfd
deepak445566 <deepakpandey1212d@gmail.com>
2025-12-15 16:21:04 +0530

    dfgh

--- .history/client/src/components/Login_20251215161814.jsx
+++ .history/client/src/components/Login_20251215161814.jsx
@@ -85,7 +85,7 @@ function Login() {
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             type="password"
             required
-            minLength="3"
+            minLength=""
             disabled={loading}
           />
         </div>
