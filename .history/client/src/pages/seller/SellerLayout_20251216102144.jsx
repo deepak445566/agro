f1e64c09256a260984ece1ea7ac1438646465f40
deepak445566 <deepakpandey1212d@gmail.com>
2025-12-16 10:30:52 +0530

    bb

--- .history/client/src/pages/seller/SellerLayout_20251216102144.jsx
+++ .history/client/src/pages/seller/SellerLayout_20251216102144.jsx
@@ -37,7 +37,7 @@ const {setIsSeler,axios,navigate,seller} =useAppContext();
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white ">
                 <Link to='/' >
-                  <img src="/logo.png" className="h-15 w-15"/>
+                  <img src=""/>
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Seller </p>
